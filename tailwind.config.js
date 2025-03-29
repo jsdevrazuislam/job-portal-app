c016e546233ab62868559740a7f670331658a446
jsdevrazuislam <razuislam.upwork@gmail.com>
2025-03-29 21:08:01 +0600

    setup project structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./**/*.{py,html}",
+  ],
+  prefix: "",
+  theme: {
+    extends: {}
+  },
+  plugins: [],
+} 
+
+export default config
+
